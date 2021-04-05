98b57715f4a43db602b054928c8f45c897ef6d11
Solan Siva <solan.siva@carleton.ca>
2021-04-04 20:37:18 -0400

    Fix elevator GUI

--- Iteration1/src/SecondSystem.java
+++ Iteration1/src/SecondSystem.java
@@ -6,6 +6,9 @@ import javax.swing.JLabel;
 import java.awt.ComponentOrientation;
 import java.awt.GridLayout;
 import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Font;
 
 
 public class SecondSystem extends JFrame {
@@ -35,25 +38,33 @@ public class SecondSystem extends JFrame {
 	 */
 	public SecondSystem(String e) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 453, 632);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(2, 1, 5, 5));
 		
-		JLabel label1 = new JLabel("Elevator "+e);
+		JLabel label1 = new JLabel(e);
+		label1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		label1.setBounds(379, 128, 52, 28);
 		label1.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		label2 = new JLabel("Elevator "+e+" Status");
-		label2.setHorizontalAlignment(SwingConstants.CENTER);
-		
 		contentPane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		contentPane.setLayout(null);
+		
+		label2 = new JLabel("Elevator "+e+" Status");
+		label2.setForeground(Color.WHITE);
+		label2.setBackground(Color.BLACK);
+		label2.setBounds(0, 11, 441, 59);
+		label2.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(label2);
 		
 		contentPane.add(label1);
-		contentPane.add(label2);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("./elevatorIteration#5.jpg"));
+		lblNewLabel.setBounds(0, 2, 470, 584);
+		contentPane.add(lblNewLabel);
 
 		contentPane.repaint();
 		contentPane.revalidate();
 	}
-	
-	
 }
